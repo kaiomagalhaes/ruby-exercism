5e011452c006121506bec0bcddaa6677b51d5bf0
Kaio Magalhães <me@kaiomagalhaes.com>
2017-04-29 10:51:16 -0300

    delete olds

--- etl/etl.rb
+++
@@ -1,8 +0,0 @@
-class ETL
-  def self.transform(old_values)
-    new_values = old_values.flat_map do |score, letters|
-      letters.collect { |letter| [letter.downcase, score] }
-    end
-    new_values.to_h
-  end
-end
