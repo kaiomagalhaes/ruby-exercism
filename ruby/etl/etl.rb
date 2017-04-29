eaff80278104f588aa1ce45468247e6d20faa2c0
Kaio Magalhães <me@kaiomagalhaes.com>
2017-04-29 20:42:46 -0300

    normalize etl

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Can be found at: http://exercism.io/submissions/0a25e94c12794edcbb412481337ba109
 class ETL
   def self.transform(old_values)
     new_values = old_values.flat_map do |score, letters|
