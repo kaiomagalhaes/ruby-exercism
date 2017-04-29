ece7132b8061eb5761deb10720723dee784e7e6f
Kaio Magalhães <me@kaiomagalhaes.com>
2017-04-29 20:33:38 -0300

    normalize binary

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Binary
   def initialize(binary_number)
     @binary_number = binary_number
@@ -10,10 +12,10 @@ class Binary
   private
 
   def do_conversion
-    return 0 unless @binary_number =~ /^[01]+*$/
+    return 0 unless @binary_number.match?(/^[01]+*$/)
     bynaries = @binary_number.reverse.each_char
     bynaries.with_index.reduce(0) do |amount, (number, index)|
-      amount + (2**index) * number.to_i
+      amount + 2**index * number.to_i
     end
   end
 end
