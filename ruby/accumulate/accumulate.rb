1edb13e73fe5eef4e516262b3b4336e1be3977dd
Kaio Magalhães <me@kaiomagalhaes.com>
2017-04-29 11:31:19 -0300

    normalize accumulate

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Can be found at: http://exercism.io/submissions/ea31a905ffc34975b541302f5089dc20
 module Enumerable
   def accumulate
     each_with_object([]) { |value, result| result << yield(value) }
